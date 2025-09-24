2a9e9aec83e8cacde3d237fb914fce94ef8df52e
Rogine <menotinfer123@gmail.com>
2025-09-24 13:43:57 -0500

    Integrated Morgan in src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 // import the express application and type definition
 import express, { Express } from "express";
 
+// Importing morgan
+import morgan from "morgan";
+
 // initialize the express application
 const app: Express = express();
+
+// Use morgan for HTTP request logging
+app.use(morgan("combined"));
 
 // respond to GET request at endpoint "/" with message
 app.get("/", (req, res) => {
