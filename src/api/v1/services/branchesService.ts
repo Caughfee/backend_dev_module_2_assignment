a7d5a296d8e0c3dae283b680cb48ce4d06b58014
Rogine <menotinfer123@gmail.com>
2025-10-05 18:47:39 -0500

    Created a service where it returns the branch by ID

--- src/api/v1/services/branchesService.ts
+++ src/api/v1/services/branchesService.ts
@@ -7,3 +7,16 @@ import { Branch, branches } from "../../../data/branches";
 export const getAllBranches = async (): Promise<Branch[]> => {
   return structuredClone(branches);
 };
+
+/**
+ * Retrieves a single branch by ID
+ * @param id - the ID for the branch
+ * @returns the ID based of the branch
+ */
+export const getBranchById = async (id: number): Promise<Branch> => {
+  const branch = branches.find((b) => b.id === id);
+  if (!branch) {
+    throw new Error(`Branch with ID ${id} not found`);
+  }
+  return structuredClone(branch);
+};
