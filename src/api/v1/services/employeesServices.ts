0f9a9a4429fbde1335e270fc0ddc4df73dc20d40
Rogine <menotinfer123@gmail.com>
2025-10-28 12:32:09 -0500

    Created a service where it retrieves a single employee by ID

--- src/api/v1/services/employeesServices.ts
+++ src/api/v1/services/employeesServices.ts
@@ -6,3 +6,16 @@ import { Employee, employees } from "../../../data/employees";
 export const getAllEmployees = async (): Promise<Employee[]> => {
   return structuredClone(employees);
 };
+
+/**
+ * Retrieves a single emp;oyee by ID
+ * @param id - the ID for the employee
+ * @returns the employee based of the ID
+ */
+export const getEmployeeById = async (id: number): Promise<Employee> => {
+  const employee = employees.find((e) => e.id === id);
+  if (!employee) {
+    throw new Error(`Employee with ID ${id} not found`);
+  }
+  return structuredClone(employee);
+};
